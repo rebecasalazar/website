a0a91eb478be83fb5d244bac2986ef38ba522463
paulosalazar <paulo.salazar@me.com>
2023-03-12 16:37:22 -0500

    init

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 if (navigator.serviceWorker) {
+
     console.log('se puede usar service worker!');
-    navigator.serviceWorker.register('/sw.js').then();
+
+    navigator.serviceWorker.register('/sw.js').then((res) => {
+
+        debugger;
+        console.log('service worker ready');
+
+    }).catch((err) => {
+
+    });
+
+    debugger;
+
+    Notification.requestPermission().then((result) => {
+        
+        console.log('notification request result');
+        debugger;
+
+    }).catch((err) => {
+
+    });
 
 }
+
--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
 
-// self.addEventListener('fetch', event => {
-//     debugger;
-//     console.log(event);
-// });
-
 self.addEventListener('install', (event) => {
 
     console.log('service worker installing...');
@@ -29,5 +24,23 @@ self.addEventListener('activate', event => {
     debugger;
     // delete old cache
     console.log('service worker is activating...');
+    console.log(event);
+});
+
+self.addEventListener('fetch', (event) => {
+    debugger;
+    console.log('fetch event:');
+    console.log(event);
+});
+
+self.addEventListener('sync', (event) => {
+    debugger;
+    console.log('sync event:');
+    console.log(event);
+});
+
+self.addEventListener('push', (event) => {
+    debugger;
+    console.log('push event:');
     console.log(event);
 });
